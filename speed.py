5ffada93f91c5a7b7bef4e4b4fa427bc67276208
shrustireddy9-png <shrustireddy9@gmail.com>
2025-11-28 11:42:42 +0530

    speed.py

--- speed.py
+++ speed.py
@@ -1,5 +1,26 @@
-def calculate_speed(distance, time):
-    speed =distance / time
-    return speed
+import sys
 
-print(calculate_speed(100, 2))
+def get_speed_info(distance, time):
+    if time == 0:
+        return "Error: Time cannot be zero."
+
+    speed = distance / time
+
+    return (
+        f"Distance: {distance}\n"
+        f"Time: {time}\n"
+        f"Speed: {speed}"
+    )
+
+
+if _name_ == "_main_":
+   
+    if len(sys.argv) == 3:
+        distance = float(sys.argv[1])
+        time = float(sys.argv[2])
+    else:
+        
+        distance = float(input("Enter distance: "))
+        time = float(input("Enter time: "))
+
+    print(get_speed_info(distance, time))
